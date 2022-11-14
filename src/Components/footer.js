bd883a90980fa4692e3cebc2684016a491619e7a
LavishRawat <lavish.rawat@coloredcow.in>
2022-11-14 17:09:44 +0530

    updated Navbar and made Footer

---
+++ src/Components/footer.js
@@ -0,0 +1,15 @@
+import Card from "react-bootstrap/card";
+import "../Scss_files/footer.css";
+
+function Footer() {
+  return (
+        <Card className='copyright footer'>
+        © 2020 Copyright:
+        <a>
+          About us
+        </a>
+      </Card>
+  );
+}
+
+export default Footer;
