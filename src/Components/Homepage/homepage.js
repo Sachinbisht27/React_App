48e7f9d3e9cc6b1c59f15c4862684b739ca9be62
LavishRawat <lavish.rawat@coloredcow.in>
2022-11-16 18:54:48 +0530

    Minor Changes

--- src/Components/Homepage/homepage.js
+++ src/Components/Homepage/homepage.js
@@ -6,7 +6,7 @@ function HomePage() {
     const isSmall = useMediaQuery({ query: '(max-width: 576px)' })
     if (isSmall) {
       return (
-        <Container className="align-mob">
+        <Container className="top-mob">
             <h1>Hire India's Top Remote Talent</h1>
             <p className='p-mob'>A hiring platform that helps remote-first compaines hire 10X faster &more efficeintly.</p>
             <p className='p-mob'>A network of 50,000+ talents, pre-vetted against their technical capabilities,language  proficiency and behavirol skills.</p>
@@ -17,8 +17,7 @@ function HomePage() {
     <Container className="align">
         <h1>Hire India's Top Remote Talent</h1>
         <p>A hiring platform that helps remote-first compaines hire 10X faster &more efficeintly.</p>
-        <p>A network of 50,000+ talents, pre-vetted against their technical capabilities,language</p>
-        <p>proficiency and behavirol skills.</p>
+        <p>A network of 50,000+ talents, pre-vetted against their technical capabilities,language proficiency and behavirol skills.</p>
     </Container>
       );
     }
