0121a6448c117d3cfc5a8a6e2e4a339be95d810f
LavishRawat <lavish.rawat@coloredcow.in>
2022-11-16 18:38:22 +0530

    Home page Content

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import NavBar from "../src/Components/Navbar/navbar"
 import Footer from "../src/Components/Footer/footer"
+import HomePage from "../src/Components/Homepage/homepage";
 
 function App() {
   return (
     <>
     <NavBar/>
+    <HomePage/>
     <Footer/>
     </>
   );
