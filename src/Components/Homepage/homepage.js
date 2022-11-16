6906088b3087a187b2118773d46f04eed67ca6ef
rohit123-rawat <rohit.rawat@coloredcow.in>
2022-11-16 23:36:59 +0530

    add some cahanges

--- src/Components/Homepage/homepage.js
+++ src/Components/Homepage/homepage.js
@@ -5,12 +5,12 @@ import { useMediaQuery } from 'react-responsive'
 function HomePage() {
   const isSmall = useMediaQuery({ query: '(max-width: 576px)' })
   if (isSmall) {
-      return (
-        <Container className="top-mob">
-            <h1>Hire India's Top Remote Talent</h1>
-            <p className='p-mob'>A hiring platform that helps remote-first companies hire 10X faster &more efficiently.</p>
-            <p className='p-mob'>A network of 50,000+ talents, pre-vetted against their technical capabilities,language proficiency and behavioral skills.</p>
-        </Container>
+    return (
+      <Container className="top-mob">
+          <h1>Hire India's Top Remote Talent</h1>
+          <p className='p-mob'>A hiring platform that helps remote-first companies hire 10X faster &more efficiently.</p>
+          <p className='p-mob'>A network of 50,000+ talents, pre-vetted against their technical capabilities,language proficiency and behavioral skills.</p>
+      </Container>
     );
   } else {
     return (
