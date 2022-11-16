4e950bcfc8bec7d8a4b0439152dbeeafd3a1897d
rohit123-rawat <rohit.rawat@coloredcow.in>
2022-11-16 17:49:27 +0530

    make suggested changes

--- src/Components/Navbar/navbar.js
+++ src/Components/Navbar/navbar.js
@@ -1,12 +1,8 @@
-import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import "../Navbar/navbar.css";
+import { Container,Form ,NavDropdown, Navbar, Nav,  } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSearch } from '@fortawesome/free-solid-svg-icons'
 import { useMediaQuery } from 'react-responsive'
+import "../Navbar/navbar.css";
 
 function NavBar() {
   const isSmall = useMediaQuery({ query: '(max-width: 576px)' })
