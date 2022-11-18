6ee92d28bd1c21baac32fb3c5510e3a58ca16340
LavishRawat <lavish.rawat@coloredcow.in>
2022-11-18 16:06:51 +0530

    Added background img

--- src/App.js
+++ src/App.js
@@ -4,19 +4,35 @@ import HomePage from "../src/Components/Homepage/homepage";
 import { Routes, Route } from "react-router-dom";
 import LoginPageContent from "../src/Components/LogInPageContent/loginpagecontent";
 import SignupPageContent from "../src/Components/SignUpPageContent/signuppagecontent";
-
+import background from "../src/img/background.png";
 
 function App() {
   return (
     <>
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<HomePage/>} ></Route>
-        <Route path="loginpagecontent" element={<LoginPageContent/>}></Route>
-        <Route path="signuppagecontent" element={<SignupPageContent/>}></Route>
-      </Routes>
-      <Footer/>
+      <div
+        style={{
+          backgroundImage: `url(${background})`,
+          height: "680px",
+          // width:"2000px",
+          backgroundSize: "round",
+          backgroundRepeat: "no-repeat",
+          // marginBottom: "200px"
+        }}
+      >
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<HomePage />}></Route>
+          <Route path="loginpagecontent" element={<LoginPageContent />}></Route>
+          <Route
+            path="signuppagecontent"
+            element={<SignupPageContent />}
+          ></Route>
+        </Routes>
+        <Footer />
+      </div>
     </>
   );
 }
+// }
+
 export default App;
