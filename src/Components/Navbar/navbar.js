e3e7557f52fc188582bccfc804419b312d6867a1
Sachin Bisht <sachinbisht27@gmail.com>
2022-11-18 23:07:07 +0530

    some changes

--- src/Components/Navbar/navbar.js
+++ src/Components/Navbar/navbar.js
@@ -51,59 +51,57 @@ function NavBar() {
     );
   } else {
     return (
-      <>
-        <Navbar expand="lg">
-          <Container fluid className="rotate">
-            <Navbar.Brand href="#">
-              <img src="logo192.png" width="40" height="40"></img>
-            </Navbar.Brand>
-            <Navbar.Toggle aria-controls="navbarScroll" />
-            <Navbar.Collapse id="navbarScroll">
-              <Nav className="navbarScroll">
-                <Nav.Link href="/" className="home nav-link nav-link-ltr">
-                  Home
-                </Nav.Link>
-                <Nav.Link href="" className="nav-link nav-link-ltr">
-                  Product
-                </Nav.Link>
-                <NavDropdown
-                  className="details"
-                  title="Details"
-                  id="navbarScrollingDropdown"
-                  onMouseLeave={() => setShowDropdown(false)}
-                  onMouseOver={() => setShowDropdown(true)}
-                  show={showDropdown}
-                >
-                  <NavDropdown.Item href="#">INFO</NavDropdown.Item>
-                  <NavDropdown.Item href="#">Sale</NavDropdown.Item>
-                  <NavDropdown.Divider />
-                  <NavDropdown.Item href="#">Profit</NavDropdown.Item>
-                </NavDropdown>
-                <Nav.Link href="#" className="nav-link nav-link-ltr">
-                  Add Product
-                </Nav.Link>
-              </Nav>
-            </Navbar.Collapse>
-            <Form className="d-flex w-30">
-              <Form.Control
-                className="searchArea"
-                type="search"
-                placeholder="Search"
-                aria-label="Search"
-              ></Form.Control>
-            </Form>
-            <button className="search">
-              <FontAwesomeIcon icon={faSearch} />
-            </button>
-            <Button href="/loginpagecontent" className="">
-              Log In
-            </Button>
-            <Button href="/signuppagecontent" className="">
-              Sign Up
-            </Button>
-          </Container>
-        </Navbar>
-      </>
+      <Navbar expand="lg">
+        <Container fluid className="rotate">
+          <Navbar.Brand href="#">
+            <img src="logo192.png" width="40" height="40"></img>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav className="navbarScroll">
+              <Nav.Link href="/" className="home nav-link nav-link-ltr">
+                Home
+              </Nav.Link>
+              <Nav.Link href="" className="nav-link nav-link-ltr">
+                Product
+              </Nav.Link>
+              <NavDropdown
+                className="details"
+                title="Details"
+                id="navbarScrollingDropdown"
+                onMouseLeave={() => setShowDropdown(false)}
+                onMouseOver={() => setShowDropdown(true)}
+                show={showDropdown}
+              >
+                <NavDropdown.Item href="#">INFO</NavDropdown.Item>
+                <NavDropdown.Item href="#">Sale</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#">Profit</NavDropdown.Item>
+              </NavDropdown>
+              <Nav.Link href="#" className="nav-link nav-link-ltr">
+                Add Product
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+          <Form className="d-flex w-30">
+            <Form.Control
+              className="searchArea"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            ></Form.Control>
+          </Form>
+          <button className="search">
+            <FontAwesomeIcon icon={faSearch} />
+          </button>
+          <Button href="/loginpagecontent" className="nav-login">
+            Log In
+          </Button>
+          <Button href="/signuppagecontent" className="nav-signup">
+            Sign Up
+          </Button>
+        </Container>
+      </Navbar>
     );
   }
 }
