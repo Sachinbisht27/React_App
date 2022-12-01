a833c3474d9da9d147e62418a38651cab9084dcf
LavishRawat <lavish.rawat@coloredcow.in>
2022-12-01 13:25:13 +0530

    connect log in to backend

--- src/Components/LogInPageContent/loginpagecontent.js
+++ src/Components/LogInPageContent/loginpagecontent.js
@@ -3,8 +3,34 @@ import "../Navbar/navbar.css";
 import { useMediaQuery } from "react-responsive";
 import { Container, Col, Form, Button } from "react-bootstrap";
 import "./loginpagecontent.css";
+import axios from "axios";
+import react, { useState} from "react";
 
 function LoginPageContent() {
+
+  const [ username, setUserName ] = useState(null);
+  const [ password, setPassword ] = useState(null);
+
+  const handleInputChange = (e) => {
+        const {id , value} = e.target;
+        if(id === "username"){
+            setUserName(value);
+        if(id === "password"){
+            setPassword(value);
+        }
+    }
+  }
+
+  const handleSubmit  = () => {
+    console.log(username,password);
+
+    axios.post(`http://localhost:9010/login`)
+      .then(res => {
+        console.log(res);
+        console.log(res.data);
+      })
+  }
+
   const isSmall = useMediaQuery({ query: "(max-width: 576px)" });
   if (isSmall) {
     return (
@@ -44,10 +70,10 @@ function LoginPageContent() {
       <>
         <img className="d-block mx-auto imgWidth" src="logo192.png"></img>
         <Col className="d-flex justify-content-center ">
-          <Form>
+          <Form onSubmit={handleSubmit}>
             <Form.Group controlId="formBasicEmail">
-              <Form.Label>Email address</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" />
+              <Form.Label>User Name</Form.Label>
+              <Form.Control value={password} onChange={(e) => handleInputChange(e)} id="username" type="text" placeholder="Enter email" />
               <Form.Text>
                 We'll never share your email with anyone else.
               </Form.Text>
@@ -55,13 +81,13 @@ function LoginPageContent() {
 
             <Form.Group controlId="formBasicPassword">
               <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" />
+              <Form.Control value={password} onChange={(e) => handleInputChange(e)} id="password" type="password" placeholder="Password" />
             </Form.Group>
             <Form.Group controlId="formBasicCheckbox">
               <Form.Check type="checkbox" label="Check me out" />
             </Form.Group>
             <Button className="signup" type="submit">
-              Sign Up
+              Log In
             </Button>
           </Form>
         </Col>
