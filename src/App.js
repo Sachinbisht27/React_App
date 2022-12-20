da171d869ee293aef7712ee8ddcd8d958909b357
Rohit Rawat <108264456+rohit123-rawat@users.noreply.github.com>
2022-12-20 23:14:19 +0530

    update the directory structure

--- src/App.js
+++ src/App.js
@@ -1,26 +1,11 @@
-import NavBar from "../src/Components/Navbar/navbar";
-import Footer from "../src/Components/Footer/footer";
-import HomePage from "../src/Components/Homepage/homepage";
-import { Routes, Route } from "react-router-dom";
-import LoginPageContent from "../src/Components/LogInPageContent/loginpagecontent";
-import SignupPageContent from "../src/Components/SignUpPageContent/signuppagecontent";
 import "../src/App.css";
 
 function App() {
   return (
     <>
-      <div className="background">
-        <NavBar />
-        <Routes>
-          <Route path="/" element={<HomePage />}></Route>
-          <Route path="loginpagecontent" element={<LoginPageContent />}></Route>
-          <Route
-            path="signuppagecontent"
-            element={<SignupPageContent />}
-          ></Route>
-        </Routes>
-        <Footer />
-      </div>
+    <div> 
+      <p>hello world</p>
+    </div>
     </>
   );
 }
